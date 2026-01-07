5f13ad0f31a930f7085babb809221222d6828f99
aaronr24468 <aaronr24468@gmail.com>
2026-01-07 12:32:42 -0600

    Fix connection parameters

--- src/functions/connectionDB.mjs
+++ src/functions/connectionDB.mjs
@@ -1,15 +1,15 @@
 import mysql from 'mysql2/promise';
-import {config} from 'dotenv';
+import { config } from 'dotenv';
 config();
 
 export const connection = mysql.createPool({
-    host: `${process.env.host}`,
-    user: `${process.env.user}`,
-    password: `${process.env.password}`,
+    host: `${process.env.HOST}`,
+    user: `${process.env.USER}`,
+    password: `${process.env.PASSWORD}`,
     database: `${process.env.DB}`,
 
-     waitForConnections: true,
-     connectionLimit: 5,
-     queueLimit: 0,
-     connectTimeout: 10000
+    waitForConnections: true,
+    connectionLimit: 5,
+    queueLimit: 0,
+    connectTimeout: 10000
 })
